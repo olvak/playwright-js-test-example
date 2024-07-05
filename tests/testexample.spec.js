ea36cda75d4f92b578f18cb6a21b163f6485963b
olvak <82042917+olvak@users.noreply.github.com>
2024-07-05 20:05:18 +0300

    add Input First and Last name

--- tests/testexample.spec.js
+++ tests/testexample.spec.js
@@ -7,4 +7,9 @@ test('Fill out the test registration form', async ({ page }) => {
 
     // Expect a title "to contain" a substring.
     await expect(page).toHaveTitle(/DEMOQA/);
+
+    // Input First and Last name
+    await expect(page.getByText('Name')).toBeVisible();
+    await page.getByPlaceholder('First Name').fill('Firstname');
+    await page.getByPlaceholder('Last Name').fill('Lastname');
 });
