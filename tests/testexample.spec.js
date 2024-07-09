5ccb5b8b623c6995913b61303962655b8dabe719
olvak <82042917+olvak@users.noreply.github.com>
2024-07-09 16:57:10 +0300

    add Select Subjects

--- tests/testexample.spec.js
+++ tests/testexample.spec.js
@@ -25,10 +25,22 @@ test('Fill out the test registration form', async ({ page }) => {
     await expect(page.getByText('Mobile')).toBeVisible();
     await page.getByPlaceholder('Mobile Number').fill('0123456789');
 
-    //Select DOB
+    // Select DOB
     await expect(page.getByText('Date of Birth')).toBeVisible();
     await page.locator('input[id="dateOfBirthInput"]').click();
     await page.selectOption('[class="react-datepicker__month-select"]', 'January');
     await page.selectOption('[class="react-datepicker__year-select"]', '2000');
     await page.locator('[aria-label="Choose Saturday, January 1st, 2000"]').click();
+
+    // Select Subjects
+    await expect(page.getByText('Subjects')).toBeVisible();
+    await page.locator('[id="subjectsContainer"]').click();
+    await page.keyboard.type('English');
+    await page.keyboard.press('Enter');
+    await page.locator('[id="subjectsContainer"]').click();
+    await page.keyboard.type('History');
+    await page.keyboard.press('Enter');
+    await page.locator('[id="subjectsContainer"]').click();
+    await page.keyboard.type('Arts');
+    await page.keyboard.press('Enter');
 });
